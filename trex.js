946c45a4224280a5b496a777f76c93251ec9a3ce
Aziz <azizjuraev9@gmail.com>
2019-09-18 17:40:39 +0500

    Update trex.js

--- trex.js
+++ trex.js
@@ -1,9 +1,10 @@
-## Trex cheat
+jdis = 110;
 r = new Runner();
 r.clearCanvas = function(){
-	if(r.horizon.obstacles.length > 0){
-		if( (r.tRex.xPos + 110) >= (r.horizon.obstacles[0].xPos + 1) && !(r.horizon.obstacles[0].typeConfig.type == "PTERODACTYL" && r.horizon.obstacles[0].yPos < 75) )
-			r.tRex.startJump(r.currentSpeed);
-	}
-	r.canvasCtx.clearRect(0, 0, r.dimensions.WIDTH, r.dimensions.HEIGHT);
+    if(r.horizon.obstacles.length > 0){
+        jdis = 110 + (r.currentSpeed * 2);
+        if( (r.tRex.xPos + jdis) >= (r.horizon.obstacles[0].xPos + 1) && !(r.horizon.obstacles[0].typeConfig.type == "PTERODACTYL" && r.horizon.obstacles[0].yPos < 75) )
+            r.tRex.startJump(r.currentSpeed);
+    }
+    r.canvasCtx.clearRect(0, 0, r.dimensions.WIDTH, r.dimensions.HEIGHT);
 };
